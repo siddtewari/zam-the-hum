468f019f004a57462a711b01d50499ed174add8c
siddtewari <sidtewari@gmail.com>
2013-03-12 07:13:05 -0400

    Initial Barebones Commit, Security Token ignored

---
+++ flashem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashem::Application.config.secret_token = '8697b2aa316ec6f58d3efb92e4ec197fcc581317b69df8ed0b4de513d3e92492bf2d93e9a2dcad0c2648a55ac81b247689c2916284e120045198ee30ca81163f'
